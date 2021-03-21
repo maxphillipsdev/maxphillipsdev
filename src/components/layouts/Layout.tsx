2259e7e3aec66944f28bda30c77f65eef9c6b580
Max Phillips <max@maxphillipsdev.com>
2021-03-21 23:43:15 +1100

    Improved header

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -7,14 +7,24 @@
 import { graphql, Link, useStaticQuery } from "gatsby";
 import React from "react";
 import MenuIcon from "../icons/Menu";
+import Img from "gatsby-image";
 
 const Layout: React.FC = ({ children }) => {
-  const { site } = useStaticQuery(graphql`
+  const { site, sanityAuthor } = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
           title
           description
+        }
+      }
+      sanityAuthor {
+        image {
+          asset {
+            fluid(maxWidth: 100) {
+              ...GatsbySanityImageFluid
+            }
+          }
         }
       }
     }
@@ -26,10 +36,21 @@ const Layout: React.FC = ({ children }) => {
     <div className="bg-gradient-to-r from-gray-100 to-red-100 min-w-screen min-h-screen">
       <div className="flex flex-col backdrop-blur">
         <header className="flex flex-nowrap justify-between items-center h-16 p-6 overflow-hidden">
-          <Link to="/">
-            <h1 className="text-4xl font-courier hover:underline">
-              {siteMetadata.title}
-            </h1>
+          <Link to="/" className="flex flex-nowrap items-center space-x-3">
+            <Img
+              fluid={sanityAuthor.image.asset.fluid}
+              className="rounded-full shadow-sm w-12 h-12"
+              title={siteMetadata.title}
+              placeholderStyle={{
+                filter: `blur(1.5rem)`,
+              }}
+            />
+            <div>
+              <h1 className="text-4xl font-courier hover:underline">
+                {siteMetadata.title}
+              </h1>
+              <h3 className="text-sm font-light">{siteMetadata.description}</h3>
+            </div>
           </Link>
           <MenuIcon />
         </header>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,29 +3,44 @@ import React from "react";
 import SEO from "../components/utils/seo";
 import { graphql, PageProps } from "gatsby";
 import PostCard from "../components/cards/PostCard";
+import { useSiteMetadata } from "../hooks/use-site-metadata";
 
 const HomePage: React.FC<PageProps> = ({ data }) => {
   const { allSanityPost } = data;
 
+  const { splashSubtitle, splashTitle } = useSiteMetadata();
+
   return (
     <Layout>
       <SEO title="Welcome!" />
-      <div className="grid grid-cols-1 grid-flow-row gap-4 flex-wrap p-3 sm:max-w-md sm:min-w-full sm:grid-cols-3">
-        {allSanityPost.nodes.map((node: any) => {
-          return (
-            <PostCard
-              title={node.title}
-              subtitle={node.subtitle}
-              key={node.slug.current}
-              image={node.mainImage.asset.fluid}
-              slug={node.slug.current}
-              categories={node.categories}
-              author={node.author}
-              date={node.publishedAt}
-            />
-          );
-        })}
-      </div>
+      <section className="h-screen w-screen flex flex-col items-center text-center sm:text-left sm:items-start justify-evenly p-3">
+        <div>
+          <h1 className="text-4xl">{splashTitle}</h1>
+          <h3 className="text-2xl">{splashSubtitle}</h3>
+        </div>
+        <span className="justify-self-end place-self-center font-bold text-xl animate-bounce rounded-full border-2 border-r-2 border-black">
+          ↓
+        </span>
+      </section>
+      <section className="p-3 space-y-3">
+        <h1 className="text-4xl font-light">Some things I've written...</h1>
+        <div className="grid grid-cols-1 grid-flow-row gap-4 flex-wrap sm:max-w-md sm:min-w-full sm:grid-cols-3">
+          {allSanityPost.nodes.map((node: any) => {
+            return (
+              <PostCard
+                title={node.title}
+                subtitle={node.subtitle}
+                key={node.slug.current}
+                image={node.mainImage.asset.fluid}
+                slug={node.slug.current}
+                categories={node.categories}
+                author={node.author}
+                date={node.publishedAt}
+              />
+            );
+          })}
+        </div>
+      </section>
     </Layout>
   );
 };
