689b94112e685590e6a962f8249a7f1fe2bcf50e
Max Phillips <max@maxphillipsdev.com>
2021-03-26 22:45:16 +1100

    Added Code component

--- src/components/posts/Code.tsx
+++ src/components/posts/Code.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import SyntaxHighlighter from "react-syntax-highlighter";
-
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 interface Props {
   node: any;
 }
@@ -9,11 +9,14 @@ const Code: React.FC<Props> = ({ node }) => {
   if (!node || !node.code) {
     return null;
   }
-
   const { language, code } = node;
   return (
     // TODO: Finish code component.
-    <SyntaxHighlighter claslanguage={language || "text"} style={}>
+    <SyntaxHighlighter
+      style={atomDark}
+      language={language ?? "text"}
+      showLineNumbers
+    >
       {code}
     </SyntaxHighlighter>
   );
