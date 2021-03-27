1005ec80a35b8b40643b132c2dc18151dfa1c401
Max Phillips <max@maxphillipsdev.com>
2021-03-27 18:32:55 +1100

    Add error boundary and sentry init

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -6,3 +6,12 @@
 
 import "@fontsource/inter";
 import "./src/css/index.css";
+
+import { init } from "@sentry/react";
+
+if (process.env.NODE_ENV === "production") {
+  init({
+    dsn: process.env.SENTRY_DSN,
+    release: process.env.COMMIT_REF || "unknown",
+  });
+}
--- web/src/components/layouts/Layout.tsx
+++ web/src/components/layouts/Layout.tsx
@@ -8,6 +8,7 @@ import { graphql, Link, useStaticQuery } from "gatsby";
 import React from "react";
 import { AiOutlineGithub } from "react-icons/ai";
 import Img from "gatsby-image";
+import { ErrorBoundary } from "@sentry/react";
 
 const Layout: React.FC = ({ children }) => {
   const { site, sanityAuthor } = useStaticQuery(graphql`
@@ -35,33 +36,36 @@ const Layout: React.FC = ({ children }) => {
   return (
     <div className="bg-gradient-to-r from-gray-50 to-yellow-100 min-w-screen min-h-screen">
       <div className="flex flex-col backdrop-blur">
-        <header className="flex flex-nowrap justify-between items-center h-16 p-6 overflow-hidden">
-          <Link to="/" className="flex flex-nowrap items-center space-x-3">
-            <Img
-              fluid={sanityAuthor.image.asset.fluid}
-              className="rounded-full shadow-sm w-12 h-12"
-              title={siteMetadata.title}
-              placeholderStyle={{
-                filter: `blur(1.5rem)`,
-              }}
-            />
-            <div>
-              <h1 className="text-4xl font-courier hover:underline">
-                {siteMetadata.title}
-              </h1>
-              <h3 className="text-sm font-light">{siteMetadata.description}</h3>
-            </div>
-          </Link>
-          <Link
-            className="transform transition-transform hover:scale-105"
-            to="https://github.com/blueybloke"
-            about="View my GitHub"
-            aria-label="View my GitHub!"
-          >
-            <AiOutlineGithub size={24} />
-          </Link>
-        </header>
-        <main>{children}</main>
+        <ErrorBoundary>
+          <header className="flex flex-nowrap justify-between items-center h-16 p-6 overflow-hidden">
+            <Link to="/" className="flex flex-nowrap items-center space-x-3">
+              <Img
+                fluid={sanityAuthor.image.asset.fluid}
+                className="rounded-full shadow-sm w-12 h-12"
+                title={siteMetadata.title}
+                placeholderStyle={{
+                  filter: `blur(1.5rem)`,
+                }}
+              />
+              <div>
+                <h1 className="text-4xl font-courier hover:underline">
+                  {siteMetadata.title}
+                </h1>
+                <h3 className="text-sm font-light">
+                  {siteMetadata.description}
+                </h3>
+              </div>
+            </Link>
+            <a
+              className="transform transition-transform hover:scale-105"
+              href="https://github.com/blueybloke"
+              aria-label="View my GitHub!"
+            >
+              <AiOutlineGithub size={24} />
+            </a>
+          </header>
+          <main>{children}</main>
+        </ErrorBoundary>
       </div>
     </div>
   );
