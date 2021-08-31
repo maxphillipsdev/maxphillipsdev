81481d5a943072322297bd4217d3efa956edcad2
Max Phillips <max@maxphillipsdev.com>
2021-09-01 01:20:48 +1000

    Add provider info

--- infra/gcp/main.tf
+++ infra/gcp/main.tf
@@ -1,5 +1,14 @@
 # Main GCP Terraform config
 
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.82.0" # Lock version
+    }
+  }
+}
+
 provider "google" {
   project = var.project_id
   region = var.region
