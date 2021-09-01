09f1d1af3d3fe11b4184850a4bad5b325edf1021
Max Phillips <max@maxphillipsdev.com>
2021-09-01 13:15:03 +1000

    Finish setting up TF

--- infra/gcp/main.tf
+++ infra/gcp/main.tf
@@ -3,7 +3,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "3.82.0" # Lock version
     }
   }
@@ -11,6 +11,6 @@ terraform {
 
 provider "google" {
   project = var.project_id
-  region = var.region
-  zone = var.zone
+  region  = var.region
+  zone    = var.zone
 }
