f5f531b0e6f08bf702c5bf45dfc023d0c95874bd
Max Phillips <max@maxphillipsdev.com>
2021-10-31 16:04:44 +1100

    Canvas testing

--- card-site/components/CardCanvas/CardCanvas.jsx
+++ card-site/components/CardCanvas/CardCanvas.jsx
@@ -1,4 +1,15 @@
+import { Canvas } from "@react-three/fiber";
+
 const CardCanvas = () => {
-  return <div></div>;
+  return (
+    <Canvas>
+      <ambientLight intensity={0.1} />
+      <directionalLight color="red" position={[0, 0, 5]} />
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial />
+      </mesh>
+    </Canvas>
+  );
 };
 export default CardCanvas;
