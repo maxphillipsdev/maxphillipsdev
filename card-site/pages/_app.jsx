28f3d8fff176901ea5ae8fe43bd41974339e20e3
Max Phillips <max@maxphillipsdev.com>
2021-10-31 15:53:56 +1100

    project setup

---
+++ card-site/pages/_app.jsx
@@ -0,0 +1,8 @@
+import "@fontsource/plus-jakarta-sans";
+import "../styles/globals.scss";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
