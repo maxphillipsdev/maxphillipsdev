6596647d5e938f9e88d59116ea2e41fe33f303cc
Max Phillips <max@maxphillipsdev.com>
2021-11-15 20:23:17 +1100

    Misc changes

--- card-site/components/CardCanvas/CardCanvas.jsx
+++ card-site/components/CardCanvas/CardCanvas.jsx
@@ -1,14 +1,54 @@
 import { Canvas } from "@react-three/fiber";
+import { Physics, usePlane, useBox } from "@react-three/cannon";
+
+function Plane(props) {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
+  return (
+    <mesh ref={ref} receiveShadow>
+      <planeBufferGeometry attach="geometry" args={[1009, 1000]} />
+      <shadowMaterial attach="material" color="#171717" />
+    </mesh>
+  );
+}
+
+function Cube(props) {
+  const [ref] = useBox(() => ({
+    mass: 1,
+    position: [0, 5, 0],
+    rotation: [0.4, 0.2, 0.5],
+    ...props,
+  }));
+  return (
+    <mesh receiveShadow castShadow ref={ref}>
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  );
+}
 
 const CardCanvas = () => {
   return (
-    <Canvas>
-      <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
+    <Canvas
+      shadowMap
+      sRGB
+      gl={{ alpha: false }}
+      camera={{ position: [-1, 2, 5], fov: 50 }}
+    >
+      <color attach="background" args={["lightblue"]} />
+      <hemisphereLight intensity={0.35} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.3}
+        penumbra={1}
+        intensity={2}
+        castShadow
+      />
+      <Physics>
+        <Plane />
+        <Cube />
+        <Cube position={[0, 0, -2]} />
+        <Cube position={[0, 20, -2]} />
+      </Physics>
     </Canvas>
   );
 };
